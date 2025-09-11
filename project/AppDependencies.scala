0fd9ff50adafe7bb6911d44d94486365a350919b
pdhiman3006 <176318726+pdhiman3006@users.noreply.github.com>
2025-09-11 14:44:19 +0100

    HMA-10715 app dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   private val bootstrapPlay28Version = "10.1.0"
   private val playHmrcApiVersion = "8.3.0"
   private val jsonJodaVersion = "2.10.7"
-  private val domainVersion = "12.1.0"
+  private val domainVersion = "13.0.0"
   private val refinedVersion = "0.11.3"
   private val taxYearVersion = "6.0.0"
 
